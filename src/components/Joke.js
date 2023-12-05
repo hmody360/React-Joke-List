842165d0d0528f1753b67f8c1bfd399c39492715
hmody360 <s441010207@st.uqu.edu.sa>
2023-12-05 19:33:06 +0300

    Feature: Added show/hide button for each Joke

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Joke = (props) => {
+    const [isShown, setIsShown] = React.useState(true)
+
+    const handleShowHide = () => {
+        setIsShown(!isShown)
+    }
         return (
             <div className="joke-container">
                 {props.setup && <h4>{props.setup}</h4>}
-                <p>{props.punchline}</p>
+                {isShown && <p>{props.punchline}</p>}
                 <p>{props.likes}</p>
+                <button className="show-hide-btn" onClick={handleShowHide}>{!isShown ? "Show" : "Hide"} Punchline</button>
             </div>
         )
 }
